a1e69cf56e885aa82c87a0327326026f8fc2782b
Cristian Leu <cristian.leu@fortech.ro>
2017-05-23 17:12:16 +0300

    set up environment

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Hai from "./testComponent/hello";
+
+ReactDOM.render(
+    <Hai />,
+    document.getElementById( "root" ),
+);
+
+console.log( "This little puggy...inside logic" );
---
+++ src/testComponent/hello.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./hello.css";
+
+const Hai = () => (
+    <h1>Hai!!!</h1>
+  );
+
+export default Hai;
